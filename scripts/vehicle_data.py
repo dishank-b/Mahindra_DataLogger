e4fdcb30daf8d2f874c47bd4bb20e9d3b0a8dde4
Dishank Bansal <dishankbansal09@gmail.com>
2016-11-08 00:35:52 +0530

    logger file changed

--- scripts/vehicle_data.py
+++ scripts/vehicle_data.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float64
-from std_msgs.msg import Twist
+from geometry_msgs.msg import Twist
 import time
-import csv
+import csv  
 
 global vel_actual
 global vel_target
@@ -12,37 +12,41 @@ global mode
 global pid_output
 global log_file
 
-def callback_va(data):
+def callback_va(data, args):
     vel_actual = data.data
-    log_file.write(time.strftime("%H:%M")+"\t\t"+str(vel_actual)+"\t\t")
+    log_file = args
+    log_file.write(time.strftime("%H:%M")+"\t\t"+str(vel_actual)+"\t\t\n")
+    rospy.loginfo("Writing Data")
 
-def callback_vt(data):
+def callback_vt(data, args):
     vel_target = data.data
+    log_file = args
     log_file.write(str(vel_target)+"\t\t")
 
-def callback_pid(data):
+def callback_pid(data, args):
     pid_output = data.linear.x
+    log_file = args
     if data.angular.y==1:
         mode = "Manual"
     else:
         mode = "Autonomous"
-    log_file.write(mode+"\n")
+    log_file.write(str(pid_output)+"\t\t"mode+"\n")
 
-def listener():
+def listener(log_file):
     rospy.init_node('data_listener', anonymous=True)
 
-    rospy.Subscriber("va", Float64, callback_va)
-    rospy.Subscriber("twist_msg", Twist, callback_pid)
-    rospy.Subscriber("vt", Float64, callback_vt)
+    rospy.Subscriber("va", Float64, callback_va, log_file)
+    rospy.Subscriber("vt", Float64, callback_vt, log_file)
+    rospy.Subscriber("twist_msg", Twist, callback_pid, log_file)
 
     rospy.spin()
 
 def initializer():
-    log_file = open("~/log_data/"+time.strftime("%H:%M")+"_"+time.strftime("%d/%m/%y")+".txt", "a")
-    log_file.write("Logging Data for"+time.strftime("%H:%M")+" "+time.strftime("%d/%m/%y")+".\n")
-    log_file.write("Time\t\tActual Velocity\t\tTarget Velocity\t\tMode")
+    log_file = open("../log_files/"+time.strftime("%d-%m-%y")+".txt", "a")
+    log_file.write("*--------Logging Data for "+time.strftime("%H:%M:%S")+" "+time.strftime("%d/%m/%y")+"-------------*\n")
+    log_file.write("Time\t\tActual Velocity\t\tTarget Velocity\t\tGiven DAC\t\tMode\n\n")
 
-    listener()
+    listener(log_file)
 
 if __name__ == '__main__':
     initializer()
